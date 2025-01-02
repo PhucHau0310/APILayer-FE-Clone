27285a7172dee74ee0944db58dc4134feb0ea155
Phuc Hau <haunhpr024@gmail.com>
2025-01-02 11:51:18 +0700

    Change

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,12 +3,6 @@
 import Header from '../../components/layouts/Header';
 import Footer from '../../components/layouts/Footer';
 import React from 'react';
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-    title: 'Authentication',
-    description: 'Marketplace for APIs',
-};
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
