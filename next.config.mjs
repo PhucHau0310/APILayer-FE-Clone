137fd618300c385755e86bafdbaa00637891cc09
Phuc Hau <haunhpr024@gmail.com>
2025-01-03 23:01:46 +0700

    config file next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
     sassOptions: {
         additionalData: `$var: red;`,
     },
+    // Tùy chọn để cấu hình việc render dynamic imports trên client-side
+    reactStrictMode: true,
+    webpack(config, { isServer }) {
+        if (!isServer) {
+            // Chạy một số điều chỉnh cho client-side build
+            config.node = {
+                fs: 'empty', // Đảm bảo rằng mô-đun 'fs' không bị yêu cầu khi build
+            };
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
