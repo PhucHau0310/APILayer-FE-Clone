cc7bbcbdefe1d8def22ef00ebb3542493fb1e2b0
Phuc Hau <haunhpr024@gmail.com>
2025-01-04 12:15:07 +0700

    Change

--- hooks/useClickOutSide.ts
+++ hooks/useClickOutSide.ts
@@ -1,3 +1,5 @@
+'use client';
+
 // hooks/useClickOutside.ts
 import { useEffect, RefObject } from 'react';
 
--- hooks/useScrollToTop.ts
+++ hooks/useScrollToTop.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 
 const useScrollToTop = (threshold = 200) => {
